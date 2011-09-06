9a0774a82cfd0a83b9f9b8f9f7c45fbdbadd7101
Sindre Sorhus <sindresorhus@gmail.com>
2011-09-07 01:37:05 +0200

    initial commit

---
+++ js/plugins.js
@@ -0,0 +1,83 @@
+window.log=function(){log.history=log.history||[];log.history.push(arguments);if(this.console){arguments.callee=arguments.callee.caller;var a=[].slice.call(arguments);(typeof console.log==="object"?log.apply.call(console.log,console,a):console.log.apply(console,a))}};
+(function(b){function c(){}for(var d="assert,count,debug,dir,dirxml,error,exception,group,groupCollapsed,groupEnd,info,log,timeStamp,profile,profileEnd,time,timeEnd,trace,warn".split(","),a;a=d.pop();){b[a]=b[a]||c}})((function(){try
+{console.log();return window.console;}catch(err){return window.console={};}})());
+
+
+function relativeTime(date_object) {
+		var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
+		var delta = parseInt((relative_to.getTime() - date_object) / 1000);
+		delta = delta + (relative_to.getTimezoneOffset() * 60);
+		if (delta < 60) {
+			return  'less than a minute ago' ;
+		} else if(delta < 120) {
+			return   'about a minute ago' ;
+		} else if(delta < (60*60)) {
+			return (parseInt(delta / 60)).toString() + '  minutes ago ';
+		} else if(delta < (120*60)) {
+			return 'about an hour ago';
+		} else if(delta < (24*60*60)) {
+			return 'about ' + (parseInt(delta / 3600)).toString() +  ' hours ago';
+		} else if(delta < (48*60*60)) {
+			return '1 day ago';
+		} else {
+			return (parseInt(delta / 86400)).toString() + ' days ago';
+		}
+}
+
+
+function linkifyTweet(text) {
+    text = text.replace(/(https?:\/\/\S+)/gi, function (s) {
+        return '<a href="' + s + '">' + s + '</a>';
+    });
+
+    text = text.replace(/(^|)@(\w+)/gi, function (s) {
+        return '<a href="http://twitter.com/' + s + '">' + s + '</a>';
+    });
+
+    text = text.replace(/(^|)#(\w+)/gi, function (s) {
+        return '<a href="http://search.twitter.com/search?q=' + s.replace(/#/,'%23') + '">' + s + '</a>';
+     });
+    return text;
+}
+
+
+$(function() {
+/*
+	// scroll spy logic
+	// ================
+	var activeTarget,
+			position = {},
+			$window = $(window),
+			nav = $('body > .topbar li a'),
+			targets = nav.map(function () {
+				return $(this).attr('href');
+			}),
+			offsets = $.map(targets, function (id) {
+				return $(id).offset().top;
+			});
+
+	function setButton(id) {
+		nav.parent("li").removeClass('active');
+		$(nav[$.inArray(id, targets)]).parent("li").addClass('active');
+	}
+
+	function processScroll(e) {
+		var scrollTop = $window.scrollTop() + 10, i;
+		for (i = offsets.length; i--;) {
+			if (activeTarget != targets[i] && scrollTop >= offsets[i] && (!offsets[i + 1] || scrollTop <= offsets[i + 1])) {
+				activeTarget = targets[i];
+				setButton(activeTarget);
+			}
+		}
+	}
+
+	nav.click(function () {
+		processScroll();
+	});
+
+	processScroll();
+
+	$window.scroll(processScroll);
+
+*/		
+});
---
+++ js/script.js
@@ -0,0 +1,92 @@
+/* Author: Sindre Sorhus
+
+*/
+
+// add abiltity to set Tipsy gravity in element data attribute
+$.fn.tipsy.elementOptions = function(elem, options) {
+	options.gravity = $(elem).data('tipsy-gravity') || options.gravity;
+    return options;
+};
+
+$(function() {
+
+	// get my repositories from GitHub
+	var $githubProjects = $('#github-projects');
+	$.getJSON('https://api.github.com/users/sindresorhus/repos?callback=?', function(response) {
+		var data = response.data,
+			output = [];
+		for ( var i = 0, len = data.length; i < len; i++ ) {
+			var item = data[i];
+			if ( item.fork ) {
+				var dateCreated = new Date( Date.parse( item.created_at ) ),
+					dateUpdated = new Date( Date.parse( item.updated_at ) ),
+					relativeCreated = relativeTime( dateCreated ),
+					relativeUpdated = relativeTime( dateUpdated );
+				output.push('<li class="tip" data-tipsy-gravity="e" title="Created ' + relativeCreated + '<br />Updated ' + relativeUpdated + '"><h4><a href="' + item.html_url + '">' + item.name + '</a></h4>');
+				output.push('<p>' + item.description + '</p></li>');
+			}
+		}
+		$githubProjects.html( output.join('') ).find('.tip').tipsy({
+			gravity: 'e',
+			offset: 4,
+			html: true
+		});
+	}).error(function() {
+		$githubProjects.html('<li>Couldn\'t load GitHub repositories.</li>');
+	});
+	
+	// get my activities from GitHub, using Google Feed proxy, since it's orginally an atom feed
+	var $githubActivities = $('#github-activities'),
+		feed = 'http://github.com/sindresorhus.atom',
+		url = 'https://ajax.googleapis.com/ajax/services/feed/load?num=8&output=json&v=1.0&q=' + encodeURIComponent( feed )
+		// only use the key when live
+		url += window.location.hostname === 'sindresorhus.com' ? '&key='+key : '',
+		url += '&callback=?';
+	$.getJSON(url, function(response) {
+		var data = response.responseData.feed.entries,
+			output = [];
+		for ( var i = 0, len = data.length; i < len; i++ ) {
+			var item = data[i],
+				date = new Date( Date.parse( item.publishedDate ) ),
+				relative = relativeTime( date ),
+				title = item.title.replace('sindresorhus ', '');
+			output.push('<li class="tip" data-tipsy2-gravity="e" title="' + relative + '"><h5><a href="' + item.link + '">' + title + '</a></h5>' + item.contentSnippet + '</li>');
+		}
+		$githubActivities.html( output.join('') ).find('.tip').tipsy({
+			gravity: 'e',
+			offset: 4
+		});
+	}).error(function() {
+		$githubActivities.html('<li>Could not load GitHub activities.</li>');
+	});
+	
+	// get my latest tweets
+	var $twitterActivities = $('#twitter-activities');
+	$.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=sindresorhus&count=10&trim_user=true&callback=?', function(response) {
+		var data = response,
+			output = [];
+		for ( var i = 0, len = data.length; i < len; i++ ) {
+			var item = data[i],
+				date = new Date( Date.parse( item.created_at ) ),
+				relative = relativeTime( date );
+			output.push('<li class="tip" data-tipsy2-gravity="w" title="' + relative + '">' + linkifyTweet(item.text) + '</li>');
+		}
+		$twitterActivities.html( output.join('') ).find('.tip').tipsy({
+			gravity: 'w',
+			offset: 4
+		});
+		
+	}).error(function() {
+		$twitterActivities.html('<li>Couldn\'t load tweets.</li>');
+	});
+	
+	/* tipsy overrides eachothers gravity if live is on, and elementOptions is overriden.
+	$('.tip').tipsy({
+		gravity: 'w',
+		//live: true,
+		html: true,
+		offset: 4
+	});
+	*/
+
+});
