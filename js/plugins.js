aba4b2e1c1207257968c546a313ca61c15e22d51
Sindre Sorhus <sindresorhus@gmail.com>
2011-10-22 02:23:49 +0200

    Update to Bootstrap 1.2
    + some cleanup.

--- js/plugins.js
+++ js/plugins.js
@@ -1,13 +1,13 @@
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
-  log.history = log.history || [];   // store logs to an array for reference
-  log.history.push(arguments);
-  if(this.console) {
-    arguments.callee = arguments.callee.caller;
-    var newarr = [].slice.call(arguments);
-    (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
-  }
+log.history = log.history || [];   // store logs to an array for reference
+log.history.push(arguments);
+if(this.console) {
+	arguments.callee = arguments.callee.caller;
+	var newarr = [].slice.call(arguments);
+	(typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
+}
 };
 
 // make it safe to use console.log always
@@ -16,81 +16,75 @@ window.log = function(){
 
 
 
+// TODO: report bug with using Twipsy live and elementOptions. Their overwriting each others option.placement. //twipsy overrides eachothers gravity if live is on, and elementOptions is overriden.
+// add abiltity to set Tipsy gravity in element data attribute
+$.fn.twipsy.elementOptions = function(elem, options) {
+    options.placement = $(elem).data('twipsy-placement') || options.placement;
+    return options;
+};
+
+// Get my GitHub repositories
+$.fn.getRepositories = function(forks) {
+	return this.each(function() {
+		// get my repositories from GitHub
+		var $this = $(this);
+		$.getJSON('https://api.github.com/users/sindresorhus/repos?callback=?', function(response) {
+			var data = response.data.reverse(),
+				output = [];
+			for ( var i = 0, len = data.length; i < len; i++ ) {
+				var item = data[i];
+				// temporary limit the number of repositories until I get enough of my own.
+				if ( i > 5 ) {
+					break;
+				}
+				if ( forks && item.fork || !forks && !item.fork ) {
+					var updated = parseToRelativeTime( item.updated_at ),
+						created = parseToRelativeTime( item.created_at ),
+						description = item.name === 'sindresorhus.github.com' ? 'The website you\'re currently viewing' : item.description;
+					output.push('<li class="tip" data-twipsy-placement="left" title="Updated ' + updated + '<br />Created ' + created + '"><h4><a href="' + item.html_url + '">' + item.name + '</a></h4><p>' + description + '</p></li>');
+				}
+			}
+			$this.html( output.join('') );
+		}).error(function() {
+			$this.html('<li>Couldn\'t load GitHub repositories.</li>');
+		});
+	});
+};
 
 function relativeTime(date_object) {
-		var relative_to = (arguments.length > 1) ? arguments[1] : new Date();
-		var delta = parseInt((relative_to.getTime() - date_object) / 1000, 10);
-		delta = delta + (relative_to.getTimezoneOffset() * 60);
-		if (delta < 60) {
-			return  'less than a minute ago' ;
-		} else if(delta < 120) {
-			return   'about a minute ago' ;
-		} else if(delta < (60*60)) {
-			return (parseInt(delta / 60, 10)).toString() + '  minutes ago ';
-		} else if(delta < (120*60)) {
-			return 'about an hour ago';
-		} else if(delta < (24*60*60)) {
-			return 'about ' + (parseInt(delta / 3600, 10)).toString() +  ' hours ago';
-		} else if(delta < (48*60*60)) {
-			return '1 day ago';
-		} else {
-			return (parseInt(delta / 86400, 10)).toString() + ' days ago';
-		}
+	var relative_to = (arguments.length > 1) ? arguments[1] : new Date(),
+		delta = parseInt((relative_to.getTime() - date_object) / 1000, 10);
+	delta = delta + (relative_to.getTimezoneOffset() * 60);
+	if (delta < 60) {
+		return  'less than a minute ago' ;
+	} else if(delta < 120) {
+		return   'about a minute ago' ;
+	} else if(delta < (60*60)) {
+		return (parseInt(delta / 60, 10)).toString() + '  minutes ago ';
+	} else if(delta < (120*60)) {
+		return 'about an hour ago';
+	} else if(delta < (24*60*60)) {
+		return 'about ' + (parseInt(delta / 3600, 10)).toString() +  ' hours ago';
+	} else if(delta < (48*60*60)) {
+		return '1 day ago';
+	} else {
+		return (parseInt(delta / 86400, 10)).toString() + ' days ago';
+	}
 }
 
+function parseToRelativeTime(string) {
+	return relativeTime( new Date( Date.parse( string ) ) );
+}
 
 function linkifyTweet(text) {
-    text = text.replace(/(https?:\/\/\S+)/gi, function (s) {
-        return '<a href="' + s + '">' + s + '</a>';
-    });
-
-    text = text.replace(/(^|)@(\w+)/gi, function (s) {
-        return '<a href="http://twitter.com/' + s + '">' + s + '</a>';
-    });
-
-    text = text.replace(/(^|)#(\w+)/gi, function (s) {
-        return '<a href="http://search.twitter.com/search?q=' + s.replace(/#/,'%23') + '">' + s + '</a>';
-     });
-    return text;
-}
-
-
-$(function() {
-	// Twitter Bootstrap
-	// scroll spy logic
-	// ================
-	var activeTarget,
-			position = {},
-			$window = $(window),
-			nav = $('body > .topbar li a'),
-			targets = nav.map(function () {
-				return $(this).attr('href');
-			}),
-			offsets = $.map(targets, function (id) {
-				return $(id).offset().top;
-			});
-
-	function setButton(id) {
-		nav.parent("li").removeClass('active');
-		$(nav[$.inArray(id, targets)]).parent("li").addClass('active');
-	}
-
-	function processScroll(e) {
-		var scrollTop = $window.scrollTop() + 10, i;
-		for (i = offsets.length; i--;) {
-			if (activeTarget != targets[i] && scrollTop >= offsets[i] && (!offsets[i + 1] || scrollTop <= offsets[i + 1])) {
-				activeTarget = targets[i];
-				setButton(activeTarget);
-			}
-		}
-	}
-
-	nav.click(function () {
-		processScroll();
+	text = text.replace(/(https?:\/\/\S+)/gi, function (s) {
+		return '<a href="' + s + '">' + s + '</a>';
 	});
-
-	processScroll();
-
-	$window.scroll(processScroll);
-	
-});
+	text = text.replace(/(^|)@(\w+)/gi, function (s) {
+		return '<a href="http://twitter.com/' + s + '">' + s + '</a>';
+	});
+	text = text.replace(/(^|)#(\w+)/gi, function (s) {
+		return '<a href="http://search.twitter.com/search?q=' + s.replace(/#/,'%23') + '">' + s + '</a>';
+	});
+	return text;
+}
--- js/script.js
+++ js/script.js
@@ -1,39 +1,11 @@
 /* Author: Sindre Sorhus
 
 */
-
-// add abiltity to set Tipsy gravity in element data attribute
-$.fn.tipsy.elementOptions = function(elem, options) {
-	options.gravity = $(elem).data('tipsy-gravity') || options.gravity;
-    return options;
-};
-
 $(function() {
 
-	// get my repositories from GitHub
-	var $githubProjects = $('#github-projects');
-	$.getJSON('https://api.github.com/users/sindresorhus/repos?callback=?', function(response) {
-		var data = response.data.reverse(),
-			output = [];
-		for ( var i = 0, len = data.length; i < len; i++ ) {
-			var item = data[i],
-				dateUpdated = new Date( Date.parse( item.updated_at ) ),
-				dateCreated = new Date( Date.parse( item.created_at ) ),
-				relativeUpdated = relativeTime( dateUpdated ),
-				relativeCreated = relativeTime( dateCreated ),
-				description = item.name === 'sindresorhus.github.com' ? 'The website you\'re currently viewing' : item.description;
-			output.push('<li class="tip" data-tipsy-gravity="e" title="Updated ' + relativeUpdated + '<br />Created ' + relativeCreated + '"><h4><a href="' + item.html_url + '">' + item.name + '</a></h4>');
-			output.push('<p>' + description + '</p></li>');
-		}
-		$githubProjects.html( output.join('') ).find('.tip').tipsy({
-			gravity: 'e',
-			offset: 4,
-			html: true
-		});
-	}).error(function() {
-		$githubProjects.html('<li>Couldn\'t load GitHub repositories.</li>');
-	});
-	
+	$('#github-repositories').getRepositories();
+	$('#github-forks').getRepositories(true);
+
 	// get my activities from GitHub, using Google Feed proxy, since it's orginally an atom feed
 	var $githubActivities = $('#github-activities'),
 		feed = 'http://github.com/sindresorhus.atom',
@@ -47,46 +19,35 @@ $(function() {
 			output = [];
 		for ( var i = 0, len = data.length; i < len; i++ ) {
 			var item = data[i],
-				date = new Date( Date.parse( item.publishedDate ) ),
-				relative = relativeTime( date ),
+				timeago = parseToRelativeTime( item.publishedDate ),
 				title = item.title.replace('sindresorhus ', '');
-			output.push('<li class="tip" data-tipsy2-gravity="e" title="' + relative + '"><h5><a href="' + item.link + '">' + title + '</a></h5>' + item.contentSnippet + '</li>');
+			output.push('<li class="tip" data-twipsy-placement="left" title="' + timeago + '"><h5><a href="' + item.link + '">' + title + '</a></h5>' + item.contentSnippet + '</li>');
 		}
-		$githubActivities.html( output.join('') ).find('.tip').tipsy({
-			gravity: 'e',
-			offset: 4
-		});
+		$githubActivities.html( output.join('') );
 	}).error(function() {
-		$githubActivities.html('<li>Could not load GitHub activities.</li>');
+		$githubActivities.html('<li>Couldn\'t load GitHub activities.</li>');
 	});
 	
 	// get my latest tweets
 	var $twitterActivities = $('#twitter-activities');
-	$.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=sindresorhus&count=13&trim_user=true&callback=?', function(response) {
+	$.getJSON('https://api.twitter.com/1/statuses/user_timeline.json?screen_name=sindresorhus&count=10&trim_user=true&callback=?', function(response) {
 		var data = response,
 			output = [];
 		for ( var i = 0, len = data.length; i < len; i++ ) {
 			var item = data[i],
-				date = new Date( Date.parse( item.created_at ) ),
-				relative = relativeTime( date );
-			output.push('<li class="tip" data-tipsy2-gravity="w" title="' + relative + '">' + linkifyTweet(item.text) + '</li>');
+				timeago = parseToRelativeTime( item.created_at ),
+				linkifiedTweet = linkifyTweet( item.text );
+			output.push('<li class="tip" data-twipsy-placement="right" title="' + timeago + '">' + linkifiedTweet + '</li>');
 		}
-		$twitterActivities.html( output.join('') ).find('.tip').tipsy({
-			gravity: 'w',
-			offset: 4
-		});
-		
+		$twitterActivities.html( output.join('') );
 	}).error(function() {
 		$twitterActivities.html('<li>Couldn\'t load tweets.</li>');
 	});
 	
-	/* tipsy overrides eachothers gravity if live is on, and elementOptions is overriden.
-	$('.tip').tipsy({
-		gravity: 'w',
-		//live: true,
+	$('.tip').twipsy({
+		live: true,
 		html: true,
 		offset: 4
 	});
-	*/
 
 });
